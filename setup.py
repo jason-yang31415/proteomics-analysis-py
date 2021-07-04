70d2d0989e69c179860468762cf4db2991ebc17c
Jason Yang <jason.yang31415@gmail.com>
2021-07-03 21:09:36 -0400

    renamed python package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='protpipe-jy',
+    name='proteomics-analysis',
     version='0.0.1',
     author='Jason Yang',
     author_email='jy31415@mit.edu',
-    description='basic proteomics pipeline to analyze LFQ data',
+    description='basic analysis for LFQ proteomics data',
     package_dir={'': './src'},
     packages=find_packages(where='./src')
 )
